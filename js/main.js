f1a09829bcfa453b0bfadcc386682ae7c3d49f7c
limeiling <limeiling@cloutropy.com>
2018-02-28 19:58:52 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import "../styles/reset.css";
+import "../styles/index.css";
+// 获取设备宽度，设置根font-size
+(function(doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function() {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      if (clientWidth) {
+        docEl.style.fontSize = clientWidth / 3.75 + 'px';
+      }
+    };
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
